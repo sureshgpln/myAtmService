3835819fb497b89d47b3427d1d980b64341b7e14
Suresh Gopalan <sureshgpln.kr@gmail.com>
2021-09-03 07:59:59 +1000

    renamed class

--- src/main/java/com/AtmCashService.java
+++ src/main/java/com/AtmCashService.java
@@ -4,17 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {JpaRepositoriesAutoConfiguration.class})
 @EnableJpaRepositories
-public class AtmCashDispenser {
+public class AtmCashService {
 
-	private static Class<AtmCashDispenser> AtmCashDispenserClass = AtmCashDispenser.class;
+	private static Class<AtmCashService> AtmCashDispenserClass = AtmCashService.class;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AtmCashDispenserClass, args);
